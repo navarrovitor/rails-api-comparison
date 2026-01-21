1b02710bcc15b0147ef668921abcc8d41b3e0fac
navarrovitor <vitor.san.nav.nexus@gmail.com>
2026-01-21 08:39:00 -0300

    add jwt authentication to prototype 2, remove .github from files

--- prototype-2-intermediate/config/routes.rb
+++ prototype-2-intermediate/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  post '/api/v1/login', to: 'api/v1/auth#login'
+
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create]
--- prototype-2-intermediate/db/schema.rb
+++ prototype-2-intermediate/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_21_110851) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_21_111313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
